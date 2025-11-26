d20d655859730312c157e74c4d35c7e578f25a40
dinellipaulo <pgsdinelli@gmail.com>
2025-11-25 23:42:51 -0400

    Implemented playGame() method

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 function main() {
-    
+    let humanScore = 0;
+    let computerScore = 0;
+
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
     }
@@ -7,7 +9,7 @@ function main() {
     function getComputerChoice() {
         const choice = getRandomInt(3);
 
-        switch(choice){
+        switch (choice) {
             case 0:
                 return 'Rock';
             case 1:
@@ -17,11 +19,42 @@ function main() {
         }
     }
 
-    function getHumanChoice(){
+    function getHumanChoice() {
         const choice = prompt('Enter your choice:');
         return choice;
     }
 
+
+    function playGame() {
+        const humanChoice = getHumanChoice().toLowerCase();
+        const computerChoice = getComputerChoice().toLowerCase();
+
+        if (humanChoice === 'rock' && computerChoice === 'scissors') {
+            humanScore++;
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            humanScore++;
+        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            humanScore++;
+        } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+            computerScore++;
+        } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            computerScore++;
+        } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+            computerScore++;
+        }
+        
+    }
+    
+    for(let i = 0; i <= 4; i++){
+        playGame();
+    }
+    
+    if (computerScore > humanScore)
+        console.log(computerScore, humanScore, 'Computer wins!');
+    else if (computerScore < humanScore)
+        console.log(computerScore, humanScore, 'You win!');
+    else
+        console.log(computerScore, humanScore, 'Tie!');
 }
 
 main();
